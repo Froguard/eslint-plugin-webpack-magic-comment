d8dfd7a7c23303848995469d38cad3f4b7f7ba4d
froguard <figure_wf@163.com>
2025-09-01 13:32:30 +0800

    feat: init

--- tests/test-cases/webpack-magic-comment.js
+++ tests/test-cases/webpack-magic-comment.js
@@ -7,24 +7,31 @@
 //------------------------------------------------------------------------------
 
 // eslint.RuleTester is string-compare ,which via error.message, so you must keep same value between errors and errMsg
-const errMsg = 'Webpack魔法注释应使用 /* */ 而不是 /** */，否则会导致编译警告: "Unexpected token"';
+const errMsg = 'Webpack魔法注释应使用 /* */ 而不是 /** */ 或 /*** */，否则会导致编译警告: "Unexpected token"';
 const errors = [{ message: errMsg}];
 
 //
 module.exports = {
     valid: [
         {
-            code: 'import(/* webpackChunkName: "someModule" */ "/a/b/c.js")', // packageName
+            code: 'import(/* webpackChunkName: "someModule" */ "/a/b/c.js")', 
         },
     ],
     invalid: [
         {
-            code: 'import(/** webpackChunkName: "someModule" */ "/a/b/c.js")', // packageName
+            code: 'import(/** webpackChunkName: "someModule" */ "/a/b/c.js")', 
             errors,
+            output: 'import(/* webpackChunkName: "someModule" */ "/a/b/c.js")',
+        },
+        {
+            code: 'import(/*** webpackChunkName: "someModule" */ "/a/b/c.js")', 
+            errors,
+            output: 'import(/* webpackChunkName: "someModule" */ "/a/b/c.js")',
         },
         // {
-        //     code: 'import(/*** webpackChunkName: "someModule" */ "/a/b/c")', // packageName
+        //     code: 'import(/* webpackChunkName: "someModule" **/"/a/b/c.js")', 
         //     errors,
+        //     output: 'import(/* webpackChunkName: "someModule" */"/a/b/c.js")',
         // },
     ]
 };
